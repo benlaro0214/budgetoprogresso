1ad584736b908a46a0932c32c3dc19314a9db2d8
Ben Larocque <benlaro0214@hotmail.com>
2021-03-23 10:53:49 -0400

    more database tweaking.

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://root:kLNmvf9TNsdtZEx@cluster0.mhkwd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
   useFindAndModify: false
 });
 
